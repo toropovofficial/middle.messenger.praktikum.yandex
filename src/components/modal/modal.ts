3cba6277ae7ea46d87a20fd983823d3b00829d2e
Aleksander <toropovofficial@yandex.ru>
2023-07-07 21:37:31 +0300

    test pre-commit

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -31,6 +31,7 @@ class Modal extends Block {
   render() {
     const pathname = $router._currentRoute?._pathname;
     const showCloseIcon = !(pathname === '/sign-in' || pathname === '/sign-up');
+
     return this.compile(template, { ...this.props, showCloseIcon });
   }
 }
